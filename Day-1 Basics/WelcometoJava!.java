d11e0592cd3eb0ab40fc30694a98abeb822475d0
ROSHI24 <roshcode24@gmail.com>
2026-08-03 23:02:23 +0530

    Hello World and Hello Java program

---
+++ Day-1 Basics/WelcometoJava!.java
@@ -0,0 +1,20 @@
+/*
+ * Problem: Hello, World.
+            Hello, Java.
+ * Platform: HackerRank
+ * Topic: Welcome to Java!
+ *
+ * Learned:
+ * - Java class structure
+ * - main() method
+ * - System.out.println()
+ * - System.out.print()
+ */
+
+public class Solution {
+
+    public static void main(String[] args) {
+       System.out.println("Hello, World.");
+       System.out.print("Hello, Java.");
+    }
+}
